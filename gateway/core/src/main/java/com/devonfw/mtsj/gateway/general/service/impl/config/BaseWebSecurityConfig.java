6d9e2fed2f6277717b43df7736d27a41fd6450ba
derochs <ochs.marcel@googlemail.com>
2020-09-28 21:47:24 +0200

    Add unprotected endpoints

--- gateway/core/src/main/java/com/devonfw/mtsj/gateway/general/service/impl/config/BaseWebSecurityConfig.java
+++ gateway/core/src/main/java/com/devonfw/mtsj/gateway/general/service/impl/config/BaseWebSecurityConfig.java
@@ -20,10 +20,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import com.devonfw.mtsj.gateway.general.common.impl.security.CsrfRequestMatcher;
 import com.devonfw.module.security.common.impl.rest.AuthenticationSuccessHandlerSendingOkHttpStatusCode;
 import com.devonfw.module.security.common.impl.rest.JsonUsernamePasswordAuthenticationFilter;
 import com.devonfw.module.security.common.impl.rest.LogoutSuccessHandlerReturningOkHttpStatusCode;
+import com.devonfw.mtsj.gateway.general.common.impl.security.CsrfRequestMatcher;
 
 /**
  * This type serves as a base class for extensions of the {@code WebSecurityConfigurerAdapter} and provides a default
@@ -67,7 +67,7 @@ public abstract class BaseWebSecurityConfig extends WebSecurityConfigurerAdapter
   public void configure(HttpSecurity http) throws Exception {
 
     String[] unsecuredResources = new String[] { "/login", "/security/**", "/services/rest/login",
-    "/services/rest/logout" };
+    "/services/rest/logout", "/api", "/frontend" };
 
     http
         //
@@ -137,8 +137,7 @@ public abstract class BaseWebSecurityConfig extends WebSecurityConfigurerAdapter
   @Inject
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
-    auth.inMemoryAuthentication().withUser("admin")
-    .password(this.passwordEncoder.encode("admin")).roles("Admin");
+    auth.inMemoryAuthentication().withUser("admin").password(this.passwordEncoder.encode("admin")).roles("Admin");
   }
 
 }
